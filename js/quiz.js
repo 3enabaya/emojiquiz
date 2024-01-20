f9e86b246913e99ab75a7cf770b42e6184d991f8
Sarrah Dawoud <92249786+3enabaya@users.noreply.github.com>
2024-01-20 18:14:08 +0100

    Add files via upload

--- js/quiz.js
+++ js/quiz.js
@@ -1,7 +1,10 @@
 let start = document.getElementById('start')
+let arabic = document.getElementById('arabic')
+
 let home = document.getElementById('quizapp')
 let quiz1 = document.getElementById('firstpage')
 let picone = document.getElementById('picone')
+let imgg = document.getElementById('img')
 let gamefinish = document.getElementById('gamefinish')
 let pictwo = document.getElementById('pictwo')
 let input = document.getElementById('input')
@@ -18,106 +21,120 @@ let win = document.getElementById('win')
 let wrong = document.getElementById('wrong')
 let fail = document.getElementById('fail')
 let audio = document.getElementById('audio')
+
+
 again.onclick= function(){
     location.reload();
 }
-start.onclick = function () {
-    // getting the game space
-    home.classList.add('hide');
-    quiz1.classList.remove('hide')
+
+
+
+
+    start.onclick = function (e) {
+        e = 'quiz.json'
+        // getting the game space
+        home.classList.add('hide');
+        quiz1.classList.remove('hide')
         // getting the data 
-    let ques = fetch('quiz.json').then(function (result) {
-        let data = result.json()
-        return data
-    }).then(function (data) {
-          // showing the data
-        let i = 0;
-        picone.setAttribute('src', `./pics/${data[i].firstpic}`)
-        pictwo.setAttribute('src', `./pics/${data[i].secondpic}`)
-        input.addEventListener("keypress", function (e) {
-            if (e.key === 'Enter') {
-                submit.click()
-}
-        })
-        submit.onclick = function () {
-            if (input.value.toLowerCase().trim().replaceAll(" ","")== data[i].answer) {
-                audio.play()
-                console.log(input.value)
-                score=10
-                sum = sum + score
-                urscore.innerHTML = `${sum}/ 100`      
-                i < data.length;
-                i++
-                console.log(input.value)
-                console.log(i)
-                picone.setAttribute('src', `./pics/${data[i].firstpic}`)
-                pictwo.setAttribute('src', `./pics/${data[i].secondpic}`)
-                input.value =" "
-                if (i=== data.length-1) {
-                    submit.classList.add('hide')
-                    finish.classList.remove('hide')
-                    skip.classList.add('hide')
-              }
-            } 
-            else {
-                wrong.play()
-                score=0
-                sum = sum + score
-                i < data.length;
-                i++
-                picone.setAttribute('src', `./pics/${data[i].firstpic}`)
-                pictwo.setAttribute('src', `./pics/${data[i].secondpic}`)
-                input.value =" "
-                if (i=== data.length-1) {
-                    submit.classList.add('hide')
-                    finish.classList.remove('hide')
-                    skip.classList.add('hide')
-                    return console.log('end')
-            } 
-            }
-        }
-        skip.onclick = function () {
-            i < data.length;
-            i++
+        let ques = fetch(e).then(function (result) {
+            let data = result.json()
+            return data
+        }).then(function (data) {
+            // showing the data
+            let i = 0;
             picone.setAttribute('src', `./pics/${data[i].firstpic}`)
-            pictwo.setAttribute('src',`./pics/${data[i].secondpic}`)
-            console.log(`i is ${i} `)
-            input.value =" "
-            if (i=== data.length-1) {
-                submit.classList.add('hide')
-                finish.classList.remove('hide')
-                skip.classList.add('hide')
-          }
-        } 
-        finish.onclick = function () {
-            if (input.value.toLowerCase().trim().replaceAll(" ","") == data[data.length - 1].answer) {
-                submit.classList.add('hide')
-                quiz1.classList.add('hide')
-                finished.classList.remove('hide')
-                score = 10
-                sum = score + sum
-                console.log('d')
-                if (sum == 100) {
-                    win.play()
-                    gamefinish.innerHTML = `Amazing !!!!!!!!!!! You Won` 
-                    
+            pictwo.setAttribute('src', `./pics/${data[i].secondpic}`)
+            input.addEventListener("keypress", function (e) {
+                if (e.key === 'Enter') {
+                    submit.click()
+                }
+            })
+            submit.onclick = function () {
+                console.log('hbala')
+                if (input.value.toLowerCase().trim().replaceAll(" ", "") == data[i].answer) {
+                    audio.play()
+                    console.log(input.value)
+                    score = 10
+                    sum = sum + score
+                    urscore.innerHTML = `${sum}/ 100`
+                    i < data.length;
+                    i++
+                    console.log(input.value)
+                    console.log(i)
+                    picone.setAttribute('src', `./pics/${data[i].firstpic}`)
+                    pictwo.setAttribute('src', `./pics/${data[i].secondpic}`)
+                    input.value = " "
+                    if (i === data.length - 1) {
+                        submit.classList.add('hide')
+                        finish.classList.remove('hide')
+                        skip.classList.add('hide')
+                    }
+                }
+                else {
+                    wrong.play()
+                    score = 0
+                    sum = sum + score
+                    i < data.length;
+                    i++
+                    picone.setAttribute('src', `./pics/${data[i].firstpic}`)
+                    pictwo.setAttribute('src', `./pics/${data[i].secondpic}`)
+                    input.value = " "
+                    if (i === data.length - 1) {
+                        submit.classList.add('hide')
+                        finish.classList.remove('hide')
+                        skip.classList.add('hide')
+                        return console.log('end')
+                    }
+                }
+            }
+            
+            skip.onclick = function () {
+                i < data.length;
+                i++
+                picone.setAttribute('src', `./pics/${data[i].firstpic}`)
+                pictwo.setAttribute('src', `./pics/${data[i].secondpic}`)
+                console.log(`i is ${i} `)
+                input.value = " "
+                if (i === data.length - 1) {
+                    submit.classList.add('hide')
+                    finish.classList.remove('hide')
+                    skip.classList.add('hide')
+                }
+            }
+            finish.onclick = function () {
+              
+                if (input.value.toLowerCase().trim().replaceAll(" ", "") == data[data.length - 1].answer) {
+                    console.log('fffffff')
+                    submit.classList.add('hide')
+                    quiz1.classList.add('hide')
+                    finished.classList.remove('hide')
+                    score = 10
+                    sum = score + sum
+                    console.log('d')
+                    if (sum = 100) {
+                        win.play()
+                        gamefinish.innerHTML = `Amazing !!!!!!!!!!! You Won`
+                        imgg.setAttribute('src', '/pics/happy.png')
+
+                    }
+                    else {
+                        fail.play()
+                        gamefinish.innerHTML = `Gameover`
+                        finalscore.innerHTML = `your score is ${sum}`
+                        imgg.setAttribute('src', './pics/crying_11570815.png')
+                    }
                 }
                 else {
                     fail.play()
-                    gamefinish.innerHTML = `Gameover`  
-                    finalscore.innerHTML = `your score is ${sum}`  
-             }
+                    gamefinish.innerHTML = `Gameover`
+                    finished.classList.remove('hide')
+                    quiz1.classList.add('hide')
+                    finalscore.innerHTML = `your score is ${sum}`
+                    imgg.setAttribute('src', './pics/crying_11570815.png')
+                    
+                }
             }
-            else {
-                    fail.play()
-                    gamefinish.innerHTML = `Gameover`  
-                  finished.classList.remove('hide')
-                  quiz1.classList.add('hide')
-                  finalscore.innerHTML = `your score is ${sum}`  
-             }
-        }
         
-    })
-  
-}
+        })
+    }
 
